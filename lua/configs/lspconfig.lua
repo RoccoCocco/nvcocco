e2c6b7a24e8c087e356259c2779efdf89413b2cc
Roko <roko.kokeza@gostudion.com>
2024-05-26 17:41:30 +0200

    feat: volar hybrid mode

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "clangd", "eslint" }
+local servers = { "html", "cssls", "clangd", "eslint" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,33 +15,24 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
 local mason_registry = require "mason-registry"
 local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
   .. "/node_modules/@vue/language-server"
 
--- Vue language options (volar)
-lspconfig.volar.setup {
-  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-  init_options = {
-    vue = {
-      hybridMode = false,
-    },
-    -- plugins = {
-    --   {
-    --     name = "@vue/typescript-plugin",
-    --     location = vue_language_server_path,
-    --     languages = { "vue" },
-    --   },
-    -- },
-  },
+lspconfig.tsserver.setup {
   on_attach = on_attach,
+  on_init = on_init,
   capabilities = capabilities,
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
+      },
+    },
+  },
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 }
--- lspconfig.pyright.setup { blabla}
+
+lspconfig.volar.setup {}

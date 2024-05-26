b8baa66b5c956802fc80a064c10ffe409ad012e8
Roko <roko.kokeza@gostudion.com>
2024-05-26 12:01:54 +0200

    feat: add lsp diagnostic float mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,4 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>lg", "<cmd> LazyGit <CR>", { desc = "Open LazyGit" })
 
+map("n", "<leader>lf", vim.diagnostic.open_float, { desc = "lsp floating diagnostics" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
